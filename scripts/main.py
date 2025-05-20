411a0df291802c7e9d45bccdff8bde92bf92edb6
Sai Madhuri Kandula <saimadhurikandula@gmail.com>
2025-05-20 10:21:46 -0700

    data extraction completed

---
+++ scripts/main.py
@@ -0,0 +1,27 @@
+from scripts.google_api import GoogleAPI
+from scripts.extract_data import YouTubeDataModel
+
+google_api = GoogleAPI()
+
+api = google_api.get_service()
+
+# List of multiple channel_ids
+channel_ids = [
+    "UCX6OQ3DkcsbYNE6H8uQQuVA" #Mr. Beast
+    
+]
+
+# Initialize the Youtube data model
+data_extraction = YouTubeDataModel(api_client=api, channel_ids=channel_ids)
+
+# === Fetch Data and Save to Files ===
+print("Fetching Channel Data...")
+data_extraction.get_channel_stats()
+
+print("Fetching Playlist Data...")
+data_extraction.get_all_playlists()
+
+print("Fetching Video Data...")
+data_extraction.get_videos_from_playlist()
+
+print("✅ All data fetched and saved successfully.")
